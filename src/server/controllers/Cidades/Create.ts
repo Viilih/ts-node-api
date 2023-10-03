a628f49ad55e4cf2495ad2f36cd1694c6cb25421
guilherme.nascimento <gnlnascimento@gmail.com>
2023-10-03 19:34:02 -0300

    feat: adding yup and started validation

--- src/server/controllers/Cidades/Create.ts
+++ src/server/controllers/Cidades/Create.ts
@@ -1,12 +1,27 @@
+import * as yup from "yup";
 import { Request, Response } from 'express';
 
 interface ICidade {
-  nome: string
+  nome: string,
 }
 
+const bodyValidation: yup.Schema<ICidade> = yup.object().shape({
+  nome: yup.string().required().min(3),
+})
 // Os type parameters passados na request ajudam a especificar algumas tipagens: nesse caso o terceiro parametro é o body
-export const create = (req: Request<{},{},ICidade>,res: Response) =>{
-  const data : ICidade = req.body; 
-  console.log(data);
+export const create = async (req: Request<{},{},ICidade>,res: Response) =>{
+  let validateTData: ICidade | undefined = undefined
+  try {
+    validateTData = await bodyValidation.validate(req.body);
+    
+  } catch (error) {
+    const yupError = error as yup.ValidationError;
+    return res.json({
+      errors: {
+        default: yupError.message
+      }
+    });
+    
+  }
   return res.send('Create!');
 };
